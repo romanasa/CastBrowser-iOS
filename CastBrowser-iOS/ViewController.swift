482398e1598d2b93e7e8fc38b8d38641697625ee
Roman Korobkov <roman.korobkov@extreme-equilibrium.com>
2025-06-08 19:52:24 +0400

    Initial Commit

---
+++ CastBrowser-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CastBrowser-iOS
+//
+//  Created by Роман Коробков on 08.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CastBrowser-iOSTests/CastBrowser_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  CastBrowser_iOSTests.swift
+//  CastBrowser-iOSTests
+//
+//  Created by Роман Коробков on 08.06.2025.
+//
+
+import Testing
+@testable import CastBrowser_iOS
+
+struct CastBrowser_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
